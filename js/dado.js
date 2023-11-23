00bbb90435bba9ea4279a1b26a9226ae87c8361a
Eduardo Nugas <81528129+eduardonugas@users.noreply.github.com>
2023-11-22 23:21:29 -0300

    Add files via upload

---
+++ js/dado.js
@@ -0,0 +1,56 @@
+// SELECIONAR ELEMENTOS
+let numeroSorteado = 0
+let imgDado = document.querySelector('#imgDado')
+let dadoRolando = document.querySelector('#dadoRolando')
+const diceBtn = document.querySelector('.diceRoll')
+
+function rollDice() {
+  hideDiceButon()
+  //Limpando ultimo número sorteado
+  imgDado.style.display = 'block'
+  imgDado.setAttribute('src', 'imgs/0.png')
+
+  // adicionar a animacao
+  imgDado.classList.add('animar')
+
+  // tocar o efeito sonoro
+  dadoRolando.play()
+
+  const randNum = getRandomInt(1, 6)
+
+  // usar setTimeout para executar as acoes apos 1.25 segundos
+  setTimeout(function () {
+    // ARMAZENAR NUMERO SORTEADO NA VARIAVEL
+    numeroSorteado = randNum
+    // definir atributo src com base no numero
+    imgDado.setAttribute('src', 'imgs/' + numeroSorteado + '.png')
+
+    // retirar a animacao
+    imgDado.classList.remove('animar')
+
+    movePlayer(turn, houses)
+    setTimeout(() => {
+      hideDice()
+    }, 2000)
+    setTimeout(() => {
+      callQuestion(players[turn].score)
+    }, 4000)
+  }, 1250)
+  return randNum
+}
+
+function hideDice() {
+  imgDado.style.display = 'none'
+}
+
+function hideDiceButon() {
+  diceBtn.style.display = 'none'
+}
+
+// FUNCAO que gera numero randomico inteiro
+// incluindo o minimo e o maximo
+function getRandomInt(min, max) {
+  min = Math.ceil(min) // arredonda para cima  ceil  = teto
+  max = Math.floor(max) // arredonda para baixo floor = piso
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
